d280ac7ddfbd4bfabc227c72032622a3f029efd3
Maree Van den Berg <mareevandenberg@Marees-MacBook-Pro.local>
2026-06-27 09:18:08 +0200

    Update Club Nanny app icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Club Nanny Service Worker - Caching for performance + push notifications
-const CACHE_NAME = 'clubnanny-v11';
+const CACHE_NAME = 'clubnanny-v12';
 const STATIC_ASSETS = [
   '/clubnannynobg.png',
   '/favicon-32.png',
